19fe3f7823f7b4adbdf9716ad733cf1c9999c951
crayolito <josalejandroapp2017@gmail.com>
2024-10-02 08:01:03 -0400

    update zzzz

--- src/app/diagramador/services/kitchensink.service.ts
+++ src/app/diagramador/services/kitchensink.service.ts
@@ -1425,105 +1425,105 @@ El proyecto se ejecutará en el puerto 8081, como se especifica en el archivo \`
 
     return `package com.nombreproyecto.proyecto.servicios;
 
-  import org.springframework.beans.factory.annotation.Autowired;
-  import org.springframework.stereotype.Service;
-  import com.nombreproyecto.proyecto.modelos.${nombreClase};
-  import com.nombreproyecto.proyecto.repositorios.${nombreClase}Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.nombreproyecto.proyecto.modelos.${nombreClase};
+import com.nombreproyecto.proyecto.repositorios.${nombreClase}Repositorio;
 
-  import java.util.List;
+import java.util.List;
 
-  @Service
-  public class ${nombreClase}Servicio {
+@Service
+public class ${nombreClase}Servicio {
 
-      @Autowired
-      private ${nombreClase}Repositorio repositorio;
+    @Autowired
+    private ${nombreClase}Repositorio repositorio;
 
-      public List<${nombreClase}> listar() {
-          return repositorio.findAll();
-      }
+    public List<${nombreClase}> listar() {
+        return repositorio.findAll();
+    }
 
-      public ${nombreClase} obtenerPorId(Long id) {
-          return repositorio.findById(id).orElse(null);
-      }
+    public ${nombreClase} obtenerPorId(Long id) {
+        return repositorio.findById(id).orElse(null);
+    }
 
-      public String guardar(${nombreClase} ${nombreClase.toLowerCase()}) {
-          repositorio.save(${nombreClase.toLowerCase()});
-          return "${nombreClase} guardado con éxito.";
-      }
+    public String guardar(${nombreClase} ${nombreClase.toLowerCase()}) {
+        repositorio.save(${nombreClase.toLowerCase()});
+        return "${nombreClase} guardado con éxito.";
+    }
 
-      public String actualizar(Long id, ${nombreClase} ${nombreClase.toLowerCase()}) {
-          if (repositorio.existsById(id)) {
-              ${nombreClase} objetoExistente = repositorio.findById(id).orElse(null);
+    public String actualizar(Long id, ${nombreClase} ${nombreClase.toLowerCase()}) {
+        if (repositorio.existsById(id)) {
+            ${nombreClase} objetoExistente = repositorio.findById(id).orElse(null);
 
-              if (objetoExistente != null) {
-                  // Actualizar atributos simples
-                  ${simples
-                    .map(
-                      (attr) =>
-                        `objetoExistente.set${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }(${nombreClase.toLowerCase()}.get${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }());`
-                    )
-                    .join('\n                ')}
+            if (objetoExistente != null) {
+                // Actualizar atributos simples
+                ${simples
+                  .map(
+                    (attr) =>
+                      `objetoExistente.set${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }(${nombreClase.toLowerCase()}.get${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }());`
+                  )
+                  .join('\n                ')}
 
-                  // Actualizar relaciones ManyToOne
-                  ${manyToOne
-                    .map(
-                      (attr) =>
-                        `objetoExistente.set${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }(${nombreClase.toLowerCase()}.get${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }());`
-                    )
-                    .join('\n                ')}
+                // Actualizar relaciones ManyToOne
+                ${manyToOne
+                  .map(
+                    (attr) =>
+                      `objetoExistente.set${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }(${nombreClase.toLowerCase()}.get${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }());`
+                  )
+                  .join('\n                ')}
 
-                  // Actualizar relaciones OneToOne
-                  ${oneToOne
-                    .map(
-                      (attr) =>
-                        `objetoExistente.set${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }(${nombreClase.toLowerCase()}.get${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }());`
-                    )
-                    .join('\n                ')}
+                // Actualizar relaciones OneToOne
+                ${oneToOne
+                  .map(
+                    (attr) =>
+                      `objetoExistente.set${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }(${nombreClase.toLowerCase()}.get${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }());`
+                  )
+                  .join('\n                ')}
 
-                  // Actualizar relaciones ManyToMany
-                  ${manyToMany
-                    .map(
-                      (attr) =>
-                        `objetoExistente.set${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }(${nombreClase.toLowerCase()}.get${
-                          attr.charAt(0).toUpperCase() + attr.slice(1)
-                        }());`
-                    )
-                    .join('\n                ')}
+                // Actualizar relaciones ManyToMany
+                ${manyToMany
+                  .map(
+                    (attr) =>
+                      `objetoExistente.set${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }(${nombreClase.toLowerCase()}.get${
+                        attr.charAt(0).toUpperCase() + attr.slice(1)
+                      }());`
+                  )
+                  .join('\n                ')}
 
-                  repositorio.save(objetoExistente);
-                  return "${nombreClase} actualizado con éxito.";
-              } else {
-                  return "${nombreClase} no encontrado.";
-              }
-          } else {
-              return "${nombreClase} no encontrado.";
-          }
-      }
+                repositorio.save(objetoExistente);
+                return "${nombreClase} actualizado con éxito.";
+            } else {
+                return "${nombreClase} no encontrado.";
+            }
+        } else {
+            return "${nombreClase} no encontrado.";
+        }
+    }
 
-      public String eliminar(Long id) {
-          if (repositorio.existsById(id)) {
-              repositorio.deleteById(id);
-              return "${nombreClase} eliminado con éxito.";
-          } else {
-              return "${nombreClase} no encontrado.";
-          }
-      }
-  }
-  `;
+    public String eliminar(Long id) {
+        if (repositorio.existsById(id)) {
+            repositorio.deleteById(id);
+            return "${nombreClase} eliminado con éxito.";
+        } else {
+            return "${nombreClase} no encontrado.";
+        }
+    }
+}
+`;
   }
 
   // Generar controlador

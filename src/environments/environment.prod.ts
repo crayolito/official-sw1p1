85b84ebbf06346e062a645cd3dcafe306dc29e65
crayolito <josalejandroapp2017@gmail.com>
2024-10-03 13:48:21 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'https://backend-sw1-19e3145c5188.herokuapp.com',
+  wsUrl: 'https://backend-sw1-19e3145c5188.herokuapp.com',
 };
 
 // apiUrl: 'https://backend-sw1-19e3145c5188.herokuapp.com',
